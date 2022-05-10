c79cee98d34e03e5200839d4534f922a20f9c173
gorolykmaxim <goroliuk.maksym@gmail.com>
2022-05-11 01:11:32 +0300

    Get rid of a RemoveAll that is not used anywhere anymore

--- src/common.h
+++ src/common.h
@@ -7,16 +7,6 @@
 #include <string>
 #include <vector>
 
-template <typename T>
-void RemoveAll(const std::vector<T>& to_remove, std::deque<T>& from) {
-    for (const T& t: to_remove) {
-        auto it = std::find(from.begin(), from.end(), t);
-        if (it != from.end()) {
-            from.erase(it);
-        }
-    }
-}
-
 template<typename T>
 bool IsCmdArgInRange(const UserCommand& cmd, const T& range) {
     return cmd.arg > 0 && cmd.arg <= range.size();
