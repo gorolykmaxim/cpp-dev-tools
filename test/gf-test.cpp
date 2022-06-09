6171c92aafa0f9594a131d9a376849b988a9d5ab
Maksym <goroliuk.maksym@gmail.com>
2022-06-09 12:23:10 +0300

    Properly remove \r\n from Output lines on Windows
    Also fix the remaining tests that were still inserting
    EOL characters to the ProcessExec.output_lines manually.

--- test/gf-test.cpp
+++ test/gf-test.cpp
@@ -12,19 +12,20 @@ TEST_F(GfTest, StartAttemptToExecuteGoogleTestsWithFilterTargetingTaskThatDoesNo
 TEST_F(GfTest, StartAndExecuteGtestTaskWithGtestFilter) {
   ProcessExec filtered_tests;
   filtered_tests.output_lines = {
-    "Running main() from /lib/gtest_main.cc\n",
-    "[==========] Running 2 tests from 1 test suite.\n",
-    "[----------] Global test environment set-up.\n",
-    "[----------] 2 tests from test_suit_1\n",
-    "[ RUN      ] test_suit_1.test1\n",
+    "Running main() from /lib/gtest_main.cc",
+    "[==========] Running 2 tests from 1 test suite.",
+    "[----------] Global test environment set-up.",
+    "[----------] 2 tests from test_suit_1",
+    "[ RUN      ] test_suit_1.test1",
     OUT_LINKS_NOT_HIGHLIGHTED(),
-    "[       OK ] test_suit_1.test1 (0 ms)\n",
-    "[ RUN      ] test_suit_1.test2\n",
-    "[       OK ] test_suit_1.test2 (0 ms)\n",
-    "[----------] 2 tests from test_suit_1 (0 ms total)\n\n",
-    "[----------] Global test environment tear-down\n",
-    "[==========] 2 tests from 1 test suite ran. (0 ms total)\n",
-    "[  PASSED  ] 2 tests.\n",
+    "[       OK ] test_suit_1.test1 (0 ms)",
+    "[ RUN      ] test_suit_1.test2",
+    "[       OK ] test_suit_1.test2 (0 ms)",
+    "[----------] 2 tests from test_suit_1 (0 ms total)",
+    "",
+    "[----------] Global test environment tear-down",
+    "[==========] 2 tests from 1 test suite ran. (0 ms total)",
+    "[  PASSED  ] 2 tests.",
   };
   mock.cmd_to_process_execs[execs.kTests + WITH_GT_FILTER("test_suit_1.*")]
       .push_back(filtered_tests);
