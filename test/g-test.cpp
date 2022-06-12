40e35ffd2004888a2e5757c03bf7efe0aefb756a
gorolykmaxim <goroliuk.maksym@gmail.com>
2022-06-12 23:38:17 +0300

    Rewrite g-test tests in a new manner

--- test/g-test.cpp
+++ test/g-test.cpp
@@ -1,47 +1,58 @@
 #include "test-lib.h"
+#include <gmock/gmock-matchers.h>
+
+using namespace testing;
 
 class Gtest: public CdtTest {};
 
 TEST_F(Gtest, StartAttemptToViewGtestTestsButSeeNoTestsHaveBeenExecutedYet) {
-  EXPECT_CDT_STARTED();
-  EXPECT_CMD("g");
+  ASSERT_CDT_STARTED();
+  CMD("g");
+  EXPECT_OUT(HasSubstr("No google tests have been executed yet."));
 }
 
 TEST_F(Gtest, StartExecuteGtestTaskTryToViewGtestTestOutputWithIndexOutOfRangeAndSeeAllTestsList) {
-  EXPECT_CDT_STARTED();
-  EXPECT_CMD("t8");
-  EXPECT_CMD("g0");
-  EXPECT_CMD("g99");
-  EXPECT_CMD("g");
+  ASSERT_CDT_STARTED();
+  CMD("t8");
+  CMD("g0");
+  EXPECT_OUT(HasSubstrsInOrder(test_list_successful));
+  CMD("g99");
+  EXPECT_OUT(HasSubstrsInOrder(test_list_successful));
+  CMD("g");
+  EXPECT_OUT(HasSubstrsInOrder(test_list_successful));
 }
 
 TEST_F(Gtest, StartExecuteGtestTaskViewGtestTaskOutputWithFileLinksHighlightedInTheOutputAndOpenLinks) {
-  EXPECT_CDT_STARTED();
-  EXPECT_CMD("t8");
-  EXPECT_CMD("g1");
-  EXPECT_OUTPUT_LINKS_TO_OPEN();
+  ASSERT_CDT_STARTED();
+  CMD("t8");
+  CMD("g1");
+  EXPECT_OUTPUT_LINKS_TO_OPEN_2();
 }
 
 TEST_F(Gtest, StartExecuteGtestTaskFailTryToViewGtestTestOutputWithIndexOutOfRangeAndSeeFailedTestsList) {
   mock.cmd_to_process_execs[execs.kTests].front() = failed_gtest_exec;
-  EXPECT_CDT_STARTED();
-  EXPECT_CMD("t8");
-  EXPECT_CMD("g0");
-  EXPECT_CMD("g99");
-  EXPECT_CMD("g");
+  ASSERT_CDT_STARTED();
+  CMD("t8");
+  CMD("g0");
+  EXPECT_OUT(HasSubstrsInOrder(test_list_failed));
+  CMD("g99");
+  EXPECT_OUT(HasSubstrsInOrder(test_list_failed));
+  CMD("g");
+  EXPECT_OUT(HasSubstrsInOrder(test_list_failed));
 }
 
 TEST_F(Gtest, StartExecuteGtestTaskFailViewGtestTestOutputWithFileLinksHighlightedInTheOutputAndOpenLinks) {
   mock.cmd_to_process_execs[execs.kTests].front() = failed_gtest_exec;
-  EXPECT_CDT_STARTED();
-  EXPECT_CMD("t8");
-  EXPECT_CMD("g1");
-  EXPECT_OUTPUT_LINKS_TO_OPEN();
+  ASSERT_CDT_STARTED();
+  CMD("t8");
+  CMD("g1");
+  EXPECT_OUTPUT_LINKS_TO_OPEN_2();
 }
 
 TEST_F(Gtest, StartExecuteGtestTaskExecuteNonGtestTaskAndDisplayOutputOfOneOfTheTestsExecutedPreviously) {
-  EXPECT_CDT_STARTED();
-  EXPECT_CMD("t8");
-  EXPECT_CMD("t1");
-  EXPECT_CMD("g1");
+  ASSERT_CDT_STARTED();
+  CMD("t8");
+  CMD("t1");
+  CMD("g1");
+  EXPECT_OUT(HasSubstr(OUT_LINKS_HIGHLIGHTED()));
 }
