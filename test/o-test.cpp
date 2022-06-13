dd9538de340e8fa290f15011cb6fd6469f284cd5
gorolykmaxim <goroliuk.maksym@gmail.com>
2022-06-13 13:30:14 +0300

    Rewrite o-test tests in a new manner

--- test/o-test.cpp
+++ test/o-test.cpp
@@ -1,4 +1,8 @@
 #include "test-lib.h"
+#include <gmock/gmock-matchers.h>
+#include <string>
+
+using namespace testing;
 
 class OTest: public CdtTest {};
 
@@ -10,73 +14,79 @@ TEST_F(OTest, StartExecuteTaskAndFailToOpenLinksFromOutput) {
   exec.exit_code = 1;
   exec.output_lines = {"failed to open file"};
   mock.cmd_to_process_execs[execs.kEditor + " /a/b/c:10"].push_back(exec);
-  EXPECT_CDT_STARTED();
-  EXPECT_CMD("t1");
-  EXPECT_CMD("o1");
+  ASSERT_CDT_STARTED();
+  CMD("t1");
+  CMD("o1");
+  EXPECT_OUT(HasSubstr(exec.output_lines.front()));
 }
 
 TEST_F(OTest, StartExecuteTaskAndOpenLinksFromOutput) {
   mock.cmd_to_process_execs[execs.kHelloWorld].front().output_lines = {
     OUT_LINKS_NOT_HIGHLIGHTED()
   };
-  EXPECT_CDT_STARTED();
-  EXPECT_CMD("t1");
-  EXPECT_OUTPUT_LINKS_TO_OPEN();
+  ASSERT_CDT_STARTED();
+  CMD("t1");
+  EXPECT_OUTPUT_LINKS_TO_OPEN_2();
 }
 
 TEST_F(OTest, StartFailToExecuteTaskWithLinksAndOpenLinksFromOutput) {
   ProcessExec& exec = mock.cmd_to_process_execs[execs.kHelloWorld].front();
   exec.exit_code = 1;
   exec.output_lines = {OUT_LINKS_NOT_HIGHLIGHTED()};
-  EXPECT_CDT_STARTED();
-  EXPECT_CMD("t1");
-  EXPECT_OUTPUT_LINKS_TO_OPEN();
+  ASSERT_CDT_STARTED();
+  CMD("t1");
+  EXPECT_OUTPUT_LINKS_TO_OPEN_2();
 }
 
 TEST_F(OTest, StartFailToExecutePreTaskOfTaskAndOpenLinksFromOutput) {
   ProcessExec& exec = mock.cmd_to_process_execs["echo pre pre task 1"].front();
   exec.exit_code = 1;
   exec.output_lines = {OUT_LINKS_NOT_HIGHLIGHTED()};
-  EXPECT_CDT_STARTED();
-  EXPECT_CMD("t3");
-  EXPECT_OUTPUT_LINKS_TO_OPEN();
+  ASSERT_CDT_STARTED();
+  CMD("t3");
+  EXPECT_OUTPUT_LINKS_TO_OPEN_2();
 }
 
 TEST_F(OTest, StartExecuteTaskWithLinksInOutputAttemptToOpenNonExistentLinkAndViewTaskOutput) {
   mock.cmd_to_process_execs[execs.kHelloWorld].front().output_lines = {
     OUT_LINKS_NOT_HIGHLIGHTED()
   };
-  EXPECT_CDT_STARTED();
-  EXPECT_CMD("t1");
-  EXPECT_LAST_EXEC_OUTPUT_DISPLAYED_ON_LINK_INDEX_OUT_OF_BOUNDS();
+  ASSERT_CDT_STARTED();
+  CMD("t1");
+  EXPECT_LAST_EXEC_OUTPUT_DISPLAYED_ON_LINK_INDEX_OUT_OF_BOUNDS_2();
 }
 
 TEST_F(OTest, StartFailToExecuteTaskWithLinksAttemptToOpenNonExistentLinkAndViewTaskOutput) {
   ProcessExec& exec = mock.cmd_to_process_execs[execs.kHelloWorld].front();
   exec.exit_code = 1;
   exec.output_lines = {OUT_LINKS_NOT_HIGHLIGHTED()};
-  EXPECT_CDT_STARTED();
-  EXPECT_CMD("t1");
-  EXPECT_LAST_EXEC_OUTPUT_DISPLAYED_ON_LINK_INDEX_OUT_OF_BOUNDS();
+  ASSERT_CDT_STARTED();
+  CMD("t1");
+  EXPECT_LAST_EXEC_OUTPUT_DISPLAYED_ON_LINK_INDEX_OUT_OF_BOUNDS_2();
 }
 
 TEST_F(OTest, StartFailToExecutePreTaskOfTaskAttemptToOpenNonExistentLinkAndViewTaskOutput) {
   ProcessExec& exec = mock.cmd_to_process_execs["echo pre pre task 1"].front();
   exec.exit_code = 1;
   exec.output_lines = {OUT_LINKS_NOT_HIGHLIGHTED()};
-  EXPECT_CDT_STARTED();
-  EXPECT_CMD("t3");
-  EXPECT_LAST_EXEC_OUTPUT_DISPLAYED_ON_LINK_INDEX_OUT_OF_BOUNDS();
+  ASSERT_CDT_STARTED();
+  CMD("t3");
+  EXPECT_LAST_EXEC_OUTPUT_DISPLAYED_ON_LINK_INDEX_OUT_OF_BOUNDS_2();
 }
 
 TEST_F(OTest, StartAttemptToOpenNonExistentLinkAndViewTaskOutput) {
-  EXPECT_CDT_STARTED();
-  EXPECT_CMD("o1");
-  EXPECT_CMD("o");
+  std::string no_file_links = "No file links in the output";
+  ASSERT_CDT_STARTED();
+  CMD("o1");
+  EXPECT_OUT(HasSubstr(no_file_links));
+  CMD("o");
+  EXPECT_OUT(HasSubstr(no_file_links));
 }
 
 TEST_F(OTest, StartAttemptToOpenLinkWhileOpenInEditorCommandIsNotSpecifiedAndSeeError) {
   mock.MockReadFile(paths.kUserConfig);
-  EXPECT_CDT_STARTED();
-  EXPECT_CMD("o");
+  ASSERT_CDT_STARTED();
+  CMD("o");
+  EXPECT_OUT(HasSubstr("'open_in_editor_command' is not specified"));
+  EXPECT_OUT(HasPath(paths.kUserConfig));
 }
