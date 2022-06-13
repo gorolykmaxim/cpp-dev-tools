f983651889d21f74daf69d215002e19aacbf2d08
gorolykmaxim <goroliuk.maksym@gmail.com>
2022-06-13 14:03:44 +0300

    Rewrite t-gtest-test tests in a new manner

--- test/t-gtest-test.cpp
+++ test/t-gtest-test.cpp
@@ -1,4 +1,9 @@
 #include "test-lib.h"
+#include <gmock/gmock-matchers.h>
+#include <string>
+#include <vector>
+
+using namespace testing;
 
 class TGtestTest: public CdtTest {};
 
@@ -9,8 +14,10 @@ TEST_F(TGtestTest, StartAndExecuteGtestTaskWithNoTests) {
     "[==========] 0 tests from 0 test suites ran. (0 ms total)",
     "[  PASSED  ] 0 tests.",
   };
-  EXPECT_CDT_STARTED();
-  EXPECT_CMD("t8");
+  ASSERT_CDT_STARTED();
+  CMD("t8");
+  EXPECT_OUT(HasSubstr(TASK_COMPLETE("run tests")));
+  EXPECT_PROCS_EXACT(execs.kTests);
 }
 
 TEST_F(TGtestTest, StartAttemptToExecuteGtestTaskWithNonExistentBinaryAndFail) {
@@ -18,71 +25,111 @@ TEST_F(TGtestTest, StartAttemptToExecuteGtestTaskWithNonExistentBinaryAndFail) {
   exec.exit_code = 127;
   exec.output_lines = {execs.kTests + " does not exist"};
   exec.stderr_lines = {0};
-  EXPECT_CDT_STARTED();
-  EXPECT_CMD("t8");
+  ASSERT_CDT_STARTED();
+  CMD("t8");
+  EXPECT_OUT(HasSubstrsInOrder(std::vector<std::string>{
+      "'" + execs.kTests + "' is not a google test executable",
+      TASK_FAILED("run tests", exec.exit_code)}));
+  EXPECT_PROCS_EXACT(execs.kTests);
 }
 
 TEST_F(TGtestTest, StartAndExecuteGtestTaskWithNonGtestBinary) {
   ProcessExec& exec = mock.cmd_to_process_execs[execs.kHelloWorld].front();
   mock.cmd_to_process_execs[execs.kTests].front() = exec;
-  EXPECT_CDT_STARTED();
-  EXPECT_CMD("t8");
+  ASSERT_CDT_STARTED();
+  CMD("t8");
+  EXPECT_OUT(HasSubstrsInOrder(std::vector<std::string>{
+      "'" + execs.kTests + "' is not a google test executable",
+      TASK_FAILED("run tests", exec.exit_code)}));
+  EXPECT_PROCS_EXACT(execs.kTests);
 }
 
 TEST_F(TGtestTest, StartExecuteGtestTaskWithNonGtestBinaryThatDoesNotFinishAndAbortItManually) {
   ProcessExec exec = mock.cmd_to_process_execs[execs.kHelloWorld].front();
   exec.is_long = true;
   mock.cmd_to_process_execs[execs.kTests].front() = exec;
-  EXPECT_CDT_STARTED();
-  EXPECT_INTERRUPTED_CMD("t8");
+  ASSERT_CDT_STARTED();
+  INTERRUPT_CMD("t8");
+  EXPECT_OUT(HasSubstrsInOrder(std::vector<std::string>{
+      "'" + execs.kTests + "' is not a google test executable",
+      TASK_FAILED("run tests", -1)}));
+  EXPECT_PROCS_EXACT(execs.kTests);
 }
 
 TEST_F(TGtestTest, StartAndExecuteGtestTaskThatExitsWith0ExitCodeInTheMiddle) {
   aborted_gtest_exec.exit_code = 0;
   mock.cmd_to_process_execs[execs.kTests].front() = aborted_gtest_exec;
-  EXPECT_CDT_STARTED();
-  EXPECT_CMD("t8");
+  ASSERT_CDT_STARTED();
+  CMD("t8");
+  EXPECT_OUT(HasSubstrsInOrder(std::vector<std::string>{
+      "Tests have finished prematurely",
+      TASK_FAILED("run tests", 0)}));
+  EXPECT_PROCS_EXACT(execs.kTests);
 }
 
 TEST_F(TGtestTest, StartAndExecuteGtestTaskThatExitsWith1ExitCodeInTheMiddle) {
   mock.cmd_to_process_execs[execs.kTests].front() = aborted_gtest_exec;
-  EXPECT_CDT_STARTED();
-  EXPECT_CMD("t8");
+  ASSERT_CDT_STARTED();
+  CMD("t8");
+  EXPECT_OUT(HasSubstrsInOrder(std::vector<std::string>{
+      "Tests have finished prematurely",
+      TASK_FAILED("run tests", aborted_gtest_exec.exit_code)}));
+  EXPECT_PROCS_EXACT(execs.kTests);
 }
 
 TEST_F(TGtestTest, StartAttemptToExecuteTaskWithGtestPreTaskThatExitsWith0ExitCodeInTheMiddleAndFail) {
   mock.cmd_to_process_execs[execs.kTests].front() = aborted_gtest_exec;
-  EXPECT_CDT_STARTED();
-  EXPECT_CMD("t9");
+  ASSERT_CDT_STARTED();
+  CMD("t9");
+  EXPECT_OUT(HasSubstrsInOrder(std::vector<std::string>{
+      "Tests have finished prematurely",
+      TASK_FAILED("run tests", aborted_gtest_exec.exit_code)}));
+  EXPECT_PROCS_EXACT(execs.kTests);
 }
 
 TEST_F(TGtestTest, StartAndExecuteGtestTaskWithMultipleSuites) {
-  EXPECT_CDT_STARTED();
-  EXPECT_CMD("t8");
+  ASSERT_CDT_STARTED();
+  CMD("t8");
+  EXPECT_OUT(HasSubstr(TASK_COMPLETE("run tests")));
+  EXPECT_PROCS_EXACT(execs.kTests);
 }
 
 TEST_F(TGtestTest, StartAndExecuteGtestTaskWithMultipleSuitesEachHavingFailedTests) {
   mock.cmd_to_process_execs[execs.kTests].front() = failed_gtest_exec;
-  EXPECT_CDT_STARTED();
-  EXPECT_CMD("t8");
+  ASSERT_CDT_STARTED();
+  CMD("t8");
+  EXPECT_OUT(HasSubstrsInOrder(test_list_failed));
+  EXPECT_OUT(HasSubstr(TASK_FAILED("run tests", failed_gtest_exec.exit_code)));
+  EXPECT_PROCS_EXACT(execs.kTests);
 }
 
 TEST_F(TGtestTest, StartAndExecuteTaskWithGtestPreTaskWithMultipleSuites) {
-  EXPECT_CDT_STARTED();
-  EXPECT_CMD("t9");
+  ASSERT_CDT_STARTED();
+  CMD("t9");
+  EXPECT_OUT(HasSubstrsInOrder(std::vector<std::string>{
+      "task with gtest pre task",
+      TASK_COMPLETE("task with gtest pre task")}));
+  EXPECT_PROCS_EXACT(execs.kTests, "echo task with gtest pre task");
 }
 
 TEST_F(TGtestTest, StartAndExecuteTaskWithGtestPreTaskWithMultipleSuitesEachHavingFailedTests) {
   mock.cmd_to_process_execs[execs.kTests].front() = failed_gtest_exec;
-  EXPECT_CDT_STARTED();
-  EXPECT_CMD("t9");
+  ASSERT_CDT_STARTED();
+  CMD("t9");
+  EXPECT_OUT(HasSubstrsInOrder(test_list_failed));
+  EXPECT_OUT(HasSubstr(TASK_FAILED("run tests", failed_gtest_exec.exit_code)));
+  EXPECT_PROCS_EXACT(execs.kTests);
 }
 
 TEST_F(TGtestTest, StartExecuteGtestTaskWithLongTestAndAbortIt) {
   aborted_gtest_exec.is_long = true;
   mock.cmd_to_process_execs[execs.kTests].front() = aborted_gtest_exec;
-  EXPECT_CDT_STARTED();
-  EXPECT_INTERRUPTED_CMD("t8");
+  ASSERT_CDT_STARTED();
+  INTERRUPT_CMD("t8");
+  EXPECT_OUT(HasSubstrsInOrder(std::vector<std::string>{
+      "Tests have finished prematurely",
+      TASK_FAILED("run tests", -1)}));
+  EXPECT_PROCS_EXACT(execs.kTests);
 }
 
 TEST_F(TGtestTest, StartExecuteGtestTaskWithFailedTestsAndLongTestAndAbortIt) {
@@ -103,18 +150,34 @@ TEST_F(TGtestTest, StartExecuteGtestTaskWithFailedTestsAndLongTestAndAbortIt) {
     "[----------] 1 test from long_tests",
     "[ RUN      ] long_tests.test1"
   };
-  EXPECT_CDT_STARTED();
-  EXPECT_INTERRUPTED_CMD("t8");
+  ASSERT_CDT_STARTED();
+  INTERRUPT_CMD("t8");
+  EXPECT_OUT(HasSubstrsInOrder(std::vector<std::string>{
+      "Tests have finished prematurely",
+      "1 \"failed_test_suit_1.test1\"",
+      "2 \"long_tests.test1\"",
+      TASK_FAILED("run tests", -1)}));
+  EXPECT_PROCS_EXACT(execs.kTests);
 }
 
 TEST_F(TGtestTest, StartExecuteGtestTaskFailOneOfTheTestsAndViewAutomaticallyDisplayedTestOutput) {
   mock.cmd_to_process_execs[execs.kTests].front() = failed_single_gtest_exec;
-  EXPECT_CDT_STARTED();
-  EXPECT_CMD("t8");
+  std::vector<std::string> expected_out = {
+      OUT_LINKS_HIGHLIGHTED(), OUT_TEST_ERROR(),
+      TASK_FAILED("run tests", failed_single_gtest_exec.exit_code)};
+  ASSERT_CDT_STARTED();
+  CMD("t8");
+  EXPECT_OUT(HasSubstrsInOrder(expected_out));
+  EXPECT_PROCS_EXACT(execs.kTests);
 }
 
 TEST_F(TGtestTest, StartExecuteTaskWithGtestPreTaskFailOneOfTheTestsAndViewAutomaticallyDisplayedTestOutput) {
   mock.cmd_to_process_execs[execs.kTests].front() = failed_single_gtest_exec;
-  EXPECT_CDT_STARTED();
-  EXPECT_CMD("t9");
+  std::vector<std::string> expected_out = {
+      OUT_LINKS_HIGHLIGHTED(), OUT_TEST_ERROR(),
+      TASK_FAILED("run tests", failed_single_gtest_exec.exit_code)};
+  ASSERT_CDT_STARTED();
+  CMD("t9");
+  EXPECT_OUT(HasSubstrsInOrder(expected_out));
+  EXPECT_PROCS_EXACT(execs.kTests);
 }
