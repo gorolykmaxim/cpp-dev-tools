176ce5797045e5dcc480c4e43522c17ba70ff6c6
gorolykmaxim <goroliuk.maksym@gmail.com>
2022-06-13 11:22:30 +0300

    Rewrite gs-test tests in a new manner

--- test/gs-test.cpp
+++ test/gs-test.cpp
@@ -1,40 +1,62 @@
 #include "test-lib.h"
+#include <gmock/gmock-matchers.h>
+
+using namespace testing;
 
 class GsTest: public CdtTest {};
 
 TEST_F(GsTest, StartAttemptToSearchGtestOutputWhenNoTestsHaveBeenExecutedYet) {
-  EXPECT_CDT_STARTED();
-  EXPECT_CMD("gs");
+  ASSERT_CDT_STARTED();
+  CMD("gs");
+  EXPECT_OUT(HasSubstr("No google tests have been executed yet."));
 }
 
 TEST_F(GsTest, StartExecuteGtestTaskWithPreTasksFailAttemptToSearchOutputOfTestThatDoesNotExistAndViewListOfFailedTests) {
   mock.cmd_to_process_execs[execs.kTests].front() = failed_gtest_exec;
-  EXPECT_CDT_STARTED();
-  EXPECT_CMD("t10");
-  EXPECT_CMD("gs");
-  EXPECT_CMD("gs0");
-  EXPECT_CMD("gs99");
+  ASSERT_CDT_STARTED();
+  CMD("t10");
+  CMD("gs");
+  EXPECT_OUT(HasSubstrsInOrder(test_list_failed));
+  CMD("gs0");
+  EXPECT_OUT(HasSubstrsInOrder(test_list_failed));
+  CMD("gs99");
+  EXPECT_OUT(HasSubstrsInOrder(test_list_failed));
 }
 
 TEST_F(GsTest, StartExecuteGtestTaskWithPreTasksFailAndSearchOutputOfTheTest) {
   mock.cmd_to_process_execs[execs.kTests].front() = failed_gtest_exec;
-  EXPECT_CDT_STARTED();
-  EXPECT_CMD("t10");
-  EXPECT_CMD("gs1\n(C\\+\\+|with description|Failure)");
-  EXPECT_CMD("gs2\n(C\\+\\+|with description|Failure)");
+  ASSERT_CDT_STARTED();
+  CMD("t10");
+  CMD("gs1\n(C\\+\\+|with description|Failure)");
+  EXPECT_OUT(HasSubstr(
+      "\x1B[35m5:\x1B[0munknown file: \x1B[32mFailure\x1B[0m\n"
+      "\x1B[35m6:\x1B[0m\x1B[32mC++\x1B[0m exception "
+      "\x1B[32mwith description\x1B[0m \"\" thrown in the test body.\n"));
+  CMD("gs2\n(C\\+\\+|with description|Failure)");
+  EXPECT_OUT(HasSubstr(
+      "\x1B[35m2:\x1B[0munknown file: \x1B[32mFailure\x1B[0m\n"
+      "\x1B[35m3:\x1B[0m\x1B[32mC++\x1B[0m exception "
+      "\x1B[32mwith description\x1B[0m \"\" thrown in the test body.\n"));
 }
 
 TEST_F(GsTest, StartExecuteGtestTaskWithPreTasksSucceedAttemptToSearchOutputOfTestThatDoesNotExistAndViewListOfAllTests) {
-  EXPECT_CDT_STARTED();
-  EXPECT_CMD("t10");
-  EXPECT_CMD("gs");
-  EXPECT_CMD("gs0");
-  EXPECT_CMD("gs99");
+  ASSERT_CDT_STARTED();
+  CMD("t10");
+  CMD("gs");
+  EXPECT_OUT(HasSubstrsInOrder(test_list_successful));
+  CMD("gs0");
+  EXPECT_OUT(HasSubstrsInOrder(test_list_successful));
+  CMD("gs99");
+  EXPECT_OUT(HasSubstrsInOrder(test_list_successful));
 }
 
 TEST_F(GsTest, StartExecuteGtestTaskWithPreTasksSucceedAndSearchOutputOfOneOfTheTests) {
-  EXPECT_CDT_STARTED();
-  EXPECT_CMD("t10");
-  EXPECT_CMD("gs1\n(some|data)");
-  EXPECT_CMD("gs2\n(some|data)");
+  ASSERT_CDT_STARTED();
+  CMD("t10");
+  CMD("gs1\n(some|data)");
+  EXPECT_OUT(HasSubstr(
+      "\x1B[35m2:\x1B[0m\x1B[32msome\x1B[0m random \x1B[32mdata\x1B[0m\n"
+      "\x1B[35m3:\x1B[0m/d/e/f:15:32 \x1B[32msome\x1B[0mthing\n"));
+  CMD("gs2\n(some|data)");
+  EXPECT_OUT(HasSubstr("No matches found"));
 }
