bcaf3f7187536669238934f1e3269eebf70cc351
gorolykmaxim <goroliuk.maksym@gmail.com>
2022-06-13 11:56:38 +0300

    Rewrite gtr-test tests in a new manner

--- test/gtr-test.cpp
+++ test/gtr-test.cpp
@@ -1,31 +1,53 @@
 #include "test-lib.h"
+#include <gmock/gmock-matchers.h>
 #include <string>
+#include <vector>
+
+using namespace testing;
 
 class GtrTest: public CdtTest {};
 
 TEST_F(GtrTest, StartRepeatedlyExecuteGtestTaskWithPreTasksUntilItFailsAndRepeatedlyRerunFailedTestUntilItFails) {
+  std::string pre_task1 = "echo pre pre task 1";
+  std::string pre_task2 = "echo pre pre task 2";
+  std::string out_links = OUT_LINKS_HIGHLIGHTED();
+  std::string task_complete = TASK_COMPLETE("failed_test_suit_1.test1");
+  std::vector<std::string> expected_out = {
+      out_links, task_complete, out_links, task_complete, out_links,
+      OUT_TEST_ERROR(), TASK_FAILED("failed_test_suit_1.test1",
+                                    failed_rerun_gtest_exec.exit_code)};
   std::string exec = execs.kTests + WITH_GT_FILTER("failed_test_suit_1.test1");
   mock.cmd_to_process_execs[exec].push_back(successful_rerun_gtest_exec);
   mock.cmd_to_process_execs[exec].push_back(successful_rerun_gtest_exec);
   mock.cmd_to_process_execs[exec].push_back(failed_rerun_gtest_exec);
   mock.cmd_to_process_execs[execs.kTests].push_back(successful_gtest_exec);
   mock.cmd_to_process_execs[execs.kTests].push_back(failed_gtest_exec);
-  EXPECT_CDT_STARTED();
-  EXPECT_CMD("tr10");
-  EXPECT_CMD("gtr1");
+  ASSERT_CDT_STARTED();
+  CMD("tr10");
+  CMD("gtr1");
+  EXPECT_OUT(HasSubstrsInOrder(expected_out));
+  EXPECT_PROCS_EXACT(pre_task1, pre_task2, execs.kTests, execs.kTests,
+                     execs.kTests, pre_task1, pre_task2, exec, exec, exec);
 }
 
 TEST_F(GtrTest, StartExecuteGtestTaskWithPreTasksSucceedRepeatedlyRerunOneOfTheTestsAndFailDueToFailedPreTask) {
+  std::string pre_task1 = "echo pre pre task 1";
+  std::string pre_task2 = "echo pre pre task 2";
   ProcessExec failed_pre_task = mock.cmd_to_process_execs["echo pre pre task 1"]
       .front();
   failed_pre_task.exit_code = 1;
   mock.cmd_to_process_execs["echo pre pre task 1"].push_back(failed_pre_task);
-  EXPECT_CDT_STARTED();
-  EXPECT_CMD("t10");
-  EXPECT_CMD("gtr1");
+  ASSERT_CDT_STARTED();
+  CMD("t10");
+  CMD("gtr1");
+  EXPECT_OUT(HasSubstr(TASK_FAILED("pre pre task 1",
+                                   failed_pre_task.exit_code)));
+  EXPECT_PROCS_EXACT(pre_task1, pre_task2, execs.kTests, pre_task1);
 }
 
 TEST_F(GtrTest, StartExecuteLongGtestTaskWithPreTasksAbortItRepeatedlyRerunFailedTestAndAbortItAgain) {
+  std::string pre_task1 = "echo pre pre task 1";
+  std::string pre_task2 = "echo pre pre task 2";
   ProcessExec rerun;
   rerun.exit_code = 1;
   rerun.is_long = true;
@@ -39,30 +61,41 @@ TEST_F(GtrTest, StartExecuteLongGtestTaskWithPreTasksAbortItRepeatedlyRerunFaile
   };
   aborted_gtest_exec.is_long = true;
   mock.cmd_to_process_execs[execs.kTests].front() = aborted_gtest_exec;
-  std::string exec = execs.kTests + WITH_GT_FILTER("exit_tests."
-                     "exit_in_the_middle");
+  std::string exec =
+      execs.kTests + WITH_GT_FILTER("exit_tests.exit_in_the_middle");
   mock.cmd_to_process_execs[exec].push_back(rerun);
-  EXPECT_CDT_STARTED();
-  EXPECT_INTERRUPTED_CMD("t10");
-  EXPECT_INTERRUPTED_CMD("gtr1");
+  ASSERT_CDT_STARTED();
+  INTERRUPT_CMD("t10");
+  INTERRUPT_CMD("gtr1");
+  EXPECT_OUT(HasSubstr(TASK_FAILED("exit_tests.exit_in_the_middle", -1)));
+  EXPECT_PROCS_EXACT(pre_task1, pre_task2, execs.kTests, pre_task1, pre_task2,
+                     exec);
 }
 
 TEST_F(GtrTest, StartAttemptToRepeatedlyRerunGtestWhenNoTestsHaveBeenExecutedYet) {
-  EXPECT_CDT_STARTED();
-  EXPECT_CMD("gtr");
+  ASSERT_CDT_STARTED();
+  CMD("gtr");
+  EXPECT_OUT(HasSubstr("No google tests have been executed yet."));
 }
 
 TEST_F(GtrTest, StartExecuteGtestTaskWithPreTasksSucceedAttemptToRepeatedlyRerunTestThatDoesNotExistAndViewListOfAllTests) {
-  EXPECT_CDT_STARTED();
-  EXPECT_CMD("t10");
-  EXPECT_CMD("gtr");
-  EXPECT_CMD("gtr0");
-  EXPECT_CMD("gtr99");
+  ASSERT_CDT_STARTED();
+  CMD("t10");
+  CMD("gtr");
+  EXPECT_OUT(HasSubstrsInOrder(test_list_successful));
+  CMD("gtr0");
+  EXPECT_OUT(HasSubstrsInOrder(test_list_successful));
+  CMD("gtr99");
+  EXPECT_OUT(HasSubstrsInOrder(test_list_successful));
+  // Check that we don't attempt to rerun any test
+  EXPECT_PROCS_EXACT("echo pre pre task 1", "echo pre pre task 2",
+                     execs.kTests);
 }
 
 TEST_F(GtrTest, StartExecuteGtestTaskRerunOneOfItsTestsAndDisplayListOfOriginallyExecutedTests) {
-  EXPECT_CDT_STARTED();
-  EXPECT_CMD("t8");
-  EXPECT_CMD("gt1");
-  EXPECT_CMD("g");
+  ASSERT_CDT_STARTED();
+  CMD("t8");
+  CMD("gt1");
+  CMD("g");
+  EXPECT_OUT(HasSubstrsInOrder(test_list_successful));
 }
