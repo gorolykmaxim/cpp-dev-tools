9fd1dce059fbf991129c100310519c08d9c33749
gorolykmaxim <goroliuk.maksym@gmail.com>
2022-06-18 22:12:01 +0300

    Rewrite gs-test in self-sufficient manner

--- test/gs-test.cpp
+++ test/gs-test.cpp
@@ -1,62 +1,81 @@
+#include "json.hpp"
 #include "test-lib.h"
 #include <gmock/gmock-matchers.h>
+#include <string>
+#include <vector>
 
 using namespace testing;
 
-class GsTest: public CdtTest {};
+class GsTest: public CdtTest {
+protected:
+  std::vector<nlohmann::json> tasks;
+  ProcessExec exec_tests_failed, exec_tests_successful;
+  std::vector<std::string> test_names;
+
+  void SetUp() override {
+    tasks = {CreateTask("run tests", "__gtest " + execs.kTests)};
+    std::vector<DummyTestSuite> suites = {
+        DummyTestSuite{"suite", {
+            DummyTest{"test1", {"some random", "data"}},
+            DummyTest{"test2", {"another", "random output"}}}}};
+    exec_tests_successful.output_lines = CreateTestOutput(suites);
+    suites[0].tests[0].is_failed = true;
+    suites[0].tests[1].is_failed = true;
+    exec_tests_failed.exit_code = 1;
+    exec_tests_failed.output_lines = CreateTestOutput(suites);
+    test_names = {"1 \"suite.test1\"", "2 \"suite.test2\""};
+    Init();
+  }
+};
 
 TEST_F(GsTest, StartAttemptToSearchGtestOutputWhenNoTestsHaveBeenExecutedYet) {
-  ASSERT_CDT_STARTED();
-  CMD("gs");
+  ASSERT_STARTED(TestCdt(tasks, {}, args));
+  RunCmd("gs");
   EXPECT_OUT(HasSubstr("No google tests have been executed yet."));
 }
 
-TEST_F(GsTest, StartExecuteGtestTaskWithPreTasksFailAttemptToSearchOutputOfTestThatDoesNotExistAndViewListOfFailedTests) {
-  mock.cmd_to_process_execs[execs.kTests].front() = failed_gtest_exec;
-  ASSERT_CDT_STARTED();
-  CMD("t10");
-  CMD("gs");
-  EXPECT_OUT(HasSubstrsInOrder(test_list_failed));
-  CMD("gs0");
-  EXPECT_OUT(HasSubstrsInOrder(test_list_failed));
-  CMD("gs99");
-  EXPECT_OUT(HasSubstrsInOrder(test_list_failed));
+TEST_F(GsTest, StartExecuteGtestTaskFailAttemptToSearchOutputOfTestThatDoesNotExistAndViewListOfFailedTests) {
+  mock.MockProc(execs.kTests, exec_tests_failed);
+  ASSERT_STARTED(TestCdt(tasks, {}, args));
+  RunCmd("t1");
+  RunCmd("gs");
+  EXPECT_OUT(HasSubstrsInOrder(test_names));
+  RunCmd("gs0");
+  EXPECT_OUT(HasSubstrsInOrder(test_names));
+  RunCmd("gs99");
+  EXPECT_OUT(HasSubstrsInOrder(test_names));
 }
 
-TEST_F(GsTest, StartExecuteGtestTaskWithPreTasksFailAndSearchOutputOfTheTest) {
-  mock.cmd_to_process_execs[execs.kTests].front() = failed_gtest_exec;
-  ASSERT_CDT_STARTED();
-  CMD("t10");
-  CMD("gs1\n(C\\+\\+|with description|Failure)");
-  EXPECT_OUT(HasSubstr(
-      "\x1B[35m5:\x1B[0munknown file: \x1B[32mFailure\x1B[0m\n"
-      "\x1B[35m6:\x1B[0m\x1B[32mC++\x1B[0m exception "
-      "\x1B[32mwith description\x1B[0m \"\" thrown in the test body.\n"));
-  CMD("gs2\n(C\\+\\+|with description|Failure)");
-  EXPECT_OUT(HasSubstr(
-      "\x1B[35m2:\x1B[0munknown file: \x1B[32mFailure\x1B[0m\n"
-      "\x1B[35m3:\x1B[0m\x1B[32mC++\x1B[0m exception "
-      "\x1B[32mwith description\x1B[0m \"\" thrown in the test body.\n"));
+TEST_F(GsTest, StartExecuteGtestTaskFailAndSearchOutputOfTheTest) {
+  mock.MockProc(execs.kTests, exec_tests_failed);
+  ASSERT_STARTED(TestCdt(tasks, {}, args));
+  RunCmd("t1");
+  RunCmd("gs1\n(some|data)");
+  EXPECT_OUT(HasSubstr("\x1B[35m1:\x1B[0m\x1B[32msome\x1B[0m random\n"
+                       "\x1B[35m2:\x1B[0m\x1B[32mdata\x1B[0m\n"));
+  RunCmd("gs2\nrandom");
+  EXPECT_OUT(HasSubstr("\x1B[35m2:\x1B[0m\x1B[32mrandom\x1B[0m output\n"));
 }
 
-TEST_F(GsTest, StartExecuteGtestTaskWithPreTasksSucceedAttemptToSearchOutputOfTestThatDoesNotExistAndViewListOfAllTests) {
-  ASSERT_CDT_STARTED();
-  CMD("t10");
-  CMD("gs");
-  EXPECT_OUT(HasSubstrsInOrder(test_list_successful));
-  CMD("gs0");
-  EXPECT_OUT(HasSubstrsInOrder(test_list_successful));
-  CMD("gs99");
-  EXPECT_OUT(HasSubstrsInOrder(test_list_successful));
+TEST_F(GsTest, StartExecuteGtestTaskSucceedAttemptToSearchOutputOfTestThatDoesNotExistAndViewListOfAllTests) {
+  mock.MockProc(execs.kTests, exec_tests_successful);
+  ASSERT_STARTED(TestCdt(tasks, {}, args));
+  RunCmd("t1");
+  RunCmd("gs");
+  EXPECT_OUT(HasSubstrsInOrder(test_names));
+  RunCmd("gs0");
+  EXPECT_OUT(HasSubstrsInOrder(test_names));
+  RunCmd("gs99");
+  EXPECT_OUT(HasSubstrsInOrder(test_names));
 }
 
-TEST_F(GsTest, StartExecuteGtestTaskWithPreTasksSucceedAndSearchOutputOfOneOfTheTests) {
-  ASSERT_CDT_STARTED();
-  CMD("t10");
+TEST_F(GsTest, StartExecuteGtestTaskSucceedAndSearchOutputOfOneOfTheTests) {
+  mock.MockProc(execs.kTests, exec_tests_successful);
+  ASSERT_STARTED(TestCdt(tasks, {}, args));
+  CMD("t1");
   CMD("gs1\n(some|data)");
-  EXPECT_OUT(HasSubstr(
-      "\x1B[35m2:\x1B[0m\x1B[32msome\x1B[0m random \x1B[32mdata\x1B[0m\n"
-      "\x1B[35m3:\x1B[0m/d/e/f:15:32 \x1B[32msome\x1B[0mthing\n"));
+  EXPECT_OUT(HasSubstr("\x1B[35m1:\x1B[0m\x1B[32msome\x1B[0m random\n"
+                       "\x1B[35m2:\x1B[0m\x1B[32mdata\x1B[0m\n"));
   CMD("gs2\n(some|data)");
   EXPECT_OUT(HasSubstr("No matches found"));
 }
