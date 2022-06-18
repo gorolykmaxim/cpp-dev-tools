d4ccc0413aae914de0c9bb333e7979791f7004cc
gorolykmaxim <goroliuk.maksym@gmail.com>
2022-06-18 21:55:55 +0300

    Rewrite gf-test in self-sufficient manner

--- test/gf-test.cpp
+++ test/gf-test.cpp
@@ -1,43 +1,47 @@
+#include "json.hpp"
 #include "test-lib.h"
 #include <gmock/gmock-matchers.h>
 #include <string>
+#include <vector>
 
 using namespace testing;
 
-class GfTest: public CdtTest {};
+class GfTest: public CdtTest {
+protected:
+  std::vector<nlohmann::json> tasks;
+
+  void SetUp() override {
+    Init();
+    tasks = {
+        CreateTaskAndProcess("pre task"),
+        CreateTask("run tests", "__gtest " + execs.kTests, {"pre task"})};
+    mock.MockReadFile(paths.kUserConfig, user_config_data.dump());
+  }
+};
 
 TEST_F(GfTest, StartAttemptToExecuteGoogleTestsWithFilterTargetingTaskThatDoesNotExistAndViewListOfTask) {
-  ASSERT_CDT_STARTED();
-  CMD("gf");
-  EXPECT_OUT(HasSubstrsInOrder(list_of_tasks_in_ui));
-  CMD("gf0");
-  EXPECT_OUT(HasSubstrsInOrder(list_of_tasks_in_ui));
-  CMD("gf99");
-  EXPECT_OUT(HasSubstrsInOrder(list_of_tasks_in_ui));
+  std::vector<std::string> list_of_tasks = {"1 \"pre task\"",
+                                            "2 \"run tests\""};
+  ASSERT_STARTED(TestCdt(tasks, {}, args));
+  RunCmd("gf");
+  EXPECT_OUT(HasSubstrsInOrder(list_of_tasks));
+  RunCmd("gf0");
+  EXPECT_OUT(HasSubstrsInOrder(list_of_tasks));
+  RunCmd("gf99");
+  EXPECT_OUT(HasSubstrsInOrder(list_of_tasks));
 }
 
-TEST_F(GfTest, StartAndExecuteGtestTaskWithGtestFilter) {
-  ProcessExec filtered_tests;
-  filtered_tests.output_lines = {
-    "Running main() from /lib/gtest_main.cc",
-    "[==========] Running 2 tests from 1 test suite.",
-    "[----------] Global test environment set-up.",
-    "[----------] 2 tests from test_suit_1",
-    "[ RUN      ] test_suit_1.test1",
-    OUT_LINKS_NOT_HIGHLIGHTED(),
-    "[       OK ] test_suit_1.test1 (0 ms)",
-    "[ RUN      ] test_suit_1.test2",
-    "[       OK ] test_suit_1.test2 (0 ms)",
-    "[----------] 2 tests from test_suit_1 (0 ms total)",
-    "",
-    "[----------] Global test environment tear-down",
-    "[==========] 2 tests from 1 test suite ran. (0 ms total)",
-    "[  PASSED  ] 2 tests.",
-  };
-  std::string cmd = execs.kTests + WITH_GT_FILTER("test_suit_1.*");
-  mock.cmd_to_process_execs[cmd].push_back(filtered_tests);
-  ASSERT_CDT_STARTED();
-  CMD("gf8\ntest_suit_1.*");
-  EXPECT_OUT(HasSubstr(TASK_COMPLETE("test_suit_1.*")));
-  EXPECT_PROCS_EXACT(cmd);
+TEST_F(GfTest, StartAndExecuteGtestTaskWithPreTasksWithGtestFilter) {
+  std::string cmd = execs.kTests + WITH_GT_FILTER("suite1.*");
+  std::vector<DummyTestSuite> suite = {
+      DummyTestSuite{"suite1", {
+          DummyTest{"test1"},
+          DummyTest{"test2"}}}};
+  ProcessExec exec;
+  exec.output_lines = CreateTestOutput(suite);
+  mock.MockProc(cmd, exec);
+  ASSERT_STARTED(TestCdt(tasks, {}, args));
+  RunCmd("gf2\nsuite1.*");
+  EXPECT_OUT(HasSubstr(TASK_COMPLETE("suite1.*")));
+  EXPECT_PROCS_EXACT(tasks[0]["command"], cmd);
 }
