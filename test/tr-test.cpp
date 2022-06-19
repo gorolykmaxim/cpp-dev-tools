60202f9f8aa53e2bc99fa9c88fd3890f64596b1a
gorolykmaxim <goroliuk.maksym@gmail.com>
2022-06-19 23:23:40 +0300

    Rewrite tr-test in self-sufficient manner

--- test/tr-test.cpp
+++ test/tr-test.cpp
@@ -5,32 +5,56 @@
 
 using namespace testing;
 
-class TrTest: public CdtTest {};
+class TrTest: public CdtTest {
+protected:
+  ProcessExec exec_tests_fail, exec_tests_success;
+
+  void SetUp() override {
+    tasks = {CreateTask("run tests", "__gtest " + execs.kTests)};
+    std::vector<DummyTestSuite> suites = {
+        DummyTestSuite{"suite", {DummyTest{"test"}}}};
+    exec_tests_success.output_lines = CreateTestOutput(suites);
+    suites[0].tests[0].is_failed = true;
+    exec_tests_fail.exit_code = 1;
+    exec_tests_fail.output_lines = CreateTestOutput(suites);
+    Init();
+  }
+};
 
 TEST_F(TrTest, StartRepeatedlyExecuteTaskUntilItFails) {
-  mock.cmd_to_process_execs[execs.kTests].push_back(successful_gtest_exec);
-  mock.cmd_to_process_execs[execs.kTests].push_back(failed_gtest_exec);
-  ASSERT_CDT_STARTED();
-  CMD("tr8");
+  mock.MockProc(execs.kTests, exec_tests_success);
+  mock.MockProc(execs.kTests, exec_tests_success);
+  mock.MockProc(execs.kTests, exec_tests_fail);
+  ASSERT_INIT_CDT();
+  RunCmd("tr1");
   EXPECT_OUT(HasSubstrsInOrder(std::vector<std::string>{
       TASK_COMPLETE("run tests"), TASK_COMPLETE("run tests"),
-      TASK_FAILED("run tests", failed_gtest_exec.exit_code)}));
+      TASK_FAILED("run tests", exec_tests_fail.exit_code)}));
   EXPECT_PROCS_EXACT(execs.kTests, execs.kTests, execs.kTests);
 }
 
 TEST_F(TrTest, StartRepeatedlyExecuteTaskUntilOneOfItsPreTasksFails) {
-  mock.cmd_to_process_execs[execs.kTests].front() = failed_gtest_exec;
-  ASSERT_CDT_STARTED();
-  CMD("tr9");
-  EXPECT_OUT(HasSubstr(TASK_FAILED("run tests", failed_gtest_exec.exit_code)));
+  tasks.push_back(CreateTaskAndProcess("primary task", {"run tests"}));
+  mock.MockProc(execs.kTests, exec_tests_fail);
+  MockTasksConfig();
+  ASSERT_INIT_CDT();
+  RunCmd("tr2");
+  EXPECT_OUT(HasSubstr(TASK_FAILED("run tests", exec_tests_fail.exit_code)));
   EXPECT_PROCS_EXACT(execs.kTests);
 }
 
 TEST_F(TrTest, StartRepeatedlyExecuteLongTaskAndAbortIt) {
-  mock.cmd_to_process_execs[execs.kHelloWorld].front().is_long = true;
-  ASSERT_CDT_STARTED();
-  INTERRUPT_CMD("tr1");
+  std::string output = "primary task";
+  std::string cmd = "echo " + output;
+  tasks = {CreateTask(output, cmd)};
+  ProcessExec exec;
+  exec.is_long = true;
+  exec.output_lines = {output};
+  mock.MockProc(cmd, exec);
+  MockTasksConfig();
+  ASSERT_INIT_CDT();
+  InterruptCmd("tr1");
   EXPECT_OUT(HasSubstrsInOrder(std::vector<std::string>{
-    "hello world!", TASK_FAILED("hello world!", -1)}));
-  EXPECT_PROCS_EXACT(execs.kHelloWorld);
+      output, TASK_FAILED("primary task", -1)}));
+  EXPECT_PROCS_EXACT(cmd);
 }
