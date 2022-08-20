dc16538e95ad1ba713f9dc96beebf34f3d4c0b04
gorolykmaxim <goroliuk.maksym@gmail.com>
2022-08-20 16:39:06 +0300

    Implement ProcessRuntime

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,25 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include "cdt.h"
+
+class MyProcess: public Process {
+public:
+  MyProcess() {
+    execute = Process::kNoopExecute;
+  }
+};
 
 int main(int argc, char** argv) {
-  QGuiApplication app(argc, argv);
+  QGuiApplication q_app(argc, argv);
   QQmlApplicationEngine engine;
   engine.load(QUrl(QStringLiteral("qrc:/cdt/qml/main.qml")));
-  return app.exec();
+  Application app;
+  app.runtime.Schedule(new MyProcess());
+  app.runtime.Schedule(new MyProcess());
+  app.runtime.ScheduleAndExecute(new MyProcess());
+  app.runtime.Schedule(new MyProcess());
+  app.runtime.Schedule(new MyProcess());
+  app.runtime.Schedule(new MyProcess());
+  app.runtime.ScheduleAndExecute(new MyProcess());
+  return q_app.exec();
 }
