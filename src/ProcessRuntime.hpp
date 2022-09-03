d1a25276eb058253692051b8c9ffbfc92f40fd3f
gorolykmaxim <goroliuk.maksym@gmail.com>
2022-09-03 19:57:30 +0300

    Get rid of "new" in ProcessRuntime.hpp

--- src/ProcessRuntime.hpp
+++ src/ProcessRuntime.hpp
@@ -53,7 +53,7 @@ public:
 
   template<typename P, typename... Args>
   QPtr<P> ScheduleRoot(Args&&... args) {
-    QPtr<P> p(new P(args...));
+    QPtr<P> p = QPtr<P>::create(args...);
     if (!free_process_ids.isEmpty()) {
       p->id = free_process_ids.pop();
       p->id.version++;
