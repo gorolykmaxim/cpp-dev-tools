034a0ba8428d498c1fb7778aa82b5d59e9c4bb67
gorolykmaxim <goroliuk.maksym@gmail.com>
2022-09-14 10:51:34 +0300

    Implement a PerfTimer utility
    Use it to quickly identify if a function runs slow to then profile
    it with a proper profiler.

---
+++ src/Common.cpp
@@ -0,0 +1,14 @@
+#include "Common.hpp"
+#include <QDebug>
+
+using namespace std::chrono;
+
+PerfTimer::PerfTimer(const QString& name)
+    : name(name),
+      start(system_clock::now()) {}
+
+PerfTimer::~PerfTimer() {
+  system_clock::time_point now = system_clock::now();
+  microseconds us = duration_cast<microseconds>(now - start);
+  qDebug() << name << "took" << us.count() << "us";
+}
--- src/Common.hpp
+++ src/Common.hpp
@@ -1,6 +1,16 @@
 #pragma once
 
+#include <chrono>
 #include <QSharedPointer>
 
 template<typename T>
 using QPtr = QSharedPointer<T>;
+
+class PerfTimer {
+public:
+  explicit PerfTimer(const QString& name);
+  ~PerfTimer();
+private:
+  QString name;
+  std::chrono::system_clock::time_point start;
+};
