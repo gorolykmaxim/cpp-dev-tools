c4f3290de0bc0f8887fb8debde5213cf9803f459
gorolykmaxim <goroliuk.maksym@gmail.com>
2023-02-04 16:44:04 +0200

    Slightly improve usability of PerfTimer

--- src/PerfTimer.cpp
+++ src/PerfTimer.cpp
@@ -1,13 +1,16 @@
 #include "PerfTimer.hpp"
 
+#define LOG() qDebug() << "[PerfTimer]"
+
 using namespace std::chrono;
 
 PerfTimer::PerfTimer(const QString& name)
-    : name(name),
-      start(system_clock::now()) {}
+    : name(name), start(system_clock::now()) {}
 
-PerfTimer::~PerfTimer() {
+void PerfTimer::Measure() const {
   system_clock::time_point now = system_clock::now();
   microseconds us = duration_cast<microseconds>(now - start);
-  qDebug() << name << "took" << us.count() << "us";
+  LOG() << name << "took" << us.count() << "us";
 }
+
+PerfTimer::~PerfTimer() { Measure(); }
--- src/PerfTimer.hpp
+++ src/PerfTimer.hpp
@@ -3,10 +3,12 @@
 #include "Lib.hpp"
 
 class PerfTimer {
-public:
-  explicit PerfTimer(const QString& name);
+ public:
+  explicit PerfTimer(const QString& name = "annonymous");
+  void Measure() const;
   ~PerfTimer();
-private:
+
+ private:
   QString name;
   std::chrono::system_clock::time_point start;
 };
