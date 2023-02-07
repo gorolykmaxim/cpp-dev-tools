27f0ecb2b93ec790233772f13098e162532a78ac
gorolykmaxim <goroliuk.maksym@gmail.com>
2023-02-07 12:56:10 +0200

    Clear list of executions when closing a project

--- src/CloseProject.cpp
+++ src/CloseProject.cpp
@@ -8,6 +8,7 @@ CloseProject::CloseProject() { EXEC_NEXT(Close); }
 
 void CloseProject::Close(AppData& app) {
   app.current_project = nullptr;
+  app.execs.clear();
   ExecDbCmdOnIOThread(app, "UPDATE project SET is_opened=false");
   ScheduleProcess<SelectProject>(app, kViewSlot);
 }
