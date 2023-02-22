bc5702cd9b44f25e7c6aad2243b5339ad36557a8
Maksym <goroliuk.maksym@gmail.com>
2023-02-22 09:38:37 +0200

    Abort InformationProvider's background task on application exit

--- src/InformationProvider.cpp
+++ src/InformationProvider.cpp
@@ -5,21 +5,30 @@
 #include <QThread>
 #include <QtConcurrent>
 
+#define EXIT_IF_SHUTDOWN() \
+  if (!QCoreApplication::instance()) return;
+
 InformationProvider::InformationProvider(QObject* parent)
     : QObject(parent), info("Hello World from provider!") {
   qDebug() << "Created";
   (void)QtConcurrent::run([this]() {
     QThread::sleep(2);
+    EXIT_IF_SHUTDOWN();
     SetInfoOnUIThread("Loading...");
     QThread::sleep(2);
+    EXIT_IF_SHUTDOWN();
     SetInfoOnUIThread("Preparing World...");
     QThread::sleep(2);
+    EXIT_IF_SHUTDOWN();
     SetInfoOnUIThread("Constructing Hello...");
     QThread::sleep(2);
+    EXIT_IF_SHUTDOWN();
     SetInfoOnUIThread("Here goes nothing...");
     QThread::sleep(2);
+    EXIT_IF_SHUTDOWN();
     SetInfoOnUIThread("...");
     QThread::sleep(2);
+    EXIT_IF_SHUTDOWN();
     SetInfoOnUIThread("Hello World!");
   });
 }
