90b44dbce1549eb3c691613b6a70ab1360dfc8f5
Maksym <goroliuk.maksym@gmail.com>
2023-03-11 14:24:54 +0200

    Get rid of export compile commands in windows build

--- build-win.bat
+++ build-win.bat
@@ -2,6 +2,6 @@
 call vcvars64.bat
 if not exist build\ (
   mkdir build
-  cmake -B build -G Ninja -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Debug
+  cmake -B build -G Ninja -DCMAKE_BUILD_TYPE=Debug
 )
 cmake --build build
