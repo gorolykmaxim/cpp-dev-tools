17708064740f16a8ab7b00fa0e2b182ee6fd09c4
Maksym <goroliuk.maksym@gmail.com>
2023-03-22 13:40:59 +0200

    Fix build-and-run-win.bat running even if build failed

--- build-and-run-win.bat
+++ build-and-run-win.bat
@@ -5,4 +5,5 @@ if not exist build\ (
   cmake -B build -G Ninja -DCMAKE_BUILD_TYPE=Debug
 )
 cmake --build build
+if %errorlevel% neq 0 exit /b %errorlevel%
 start /b /wait ./build/cpp-dev-tools.exe
