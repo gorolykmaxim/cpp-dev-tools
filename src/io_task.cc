84dbfb9b87370870d1d303dc5471c48b73114b1d
gorolykmaxim <goroliuk.maksym@gmail.com>
2023-05-20 13:35:01 +0300

    Make IoTask use Qt's OOB concurrency primitives

--- src/io_task.cc
+++ src/io_task.cc
@@ -1,33 +1,14 @@
 #include "io_task.h"
 
-#include "application.h"
-
-BaseIoTask::BaseIoTask() : QObject(nullptr) {}
-
-void BaseIoTask::Run() {
-  (void)QtConcurrent::run(&Application::Get().io_thread_pool, [this] {
-    RunInBackground();
-    emit finished();
-    deleteLater();
-  });
-}
-
-class VoidIoTask : public BaseIoTask {
- public:
-  explicit VoidIoTask(const std::function<void()> &on_background)
-      : BaseIoTask(), on_background(on_background) {}
-
- protected:
-  void RunInBackground() { on_background(); }
-
- private:
-  std::function<void()> on_background;
-};
-
 void IoTask::Run(QObject *requestor, const std::function<void()> &on_io_thread,
                  const std::function<void()> &on_ui_thread) {
-  VoidIoTask *task = new VoidIoTask(on_io_thread);
-  QObject::connect(task, &BaseIoTask::finished, requestor,
-                   [on_ui_thread] { on_ui_thread(); });
-  task->Run();
+  Application &app = Application::Get();
+  auto watcher = new QFutureWatcher<void>();
+  QObject::connect(watcher, &QFutureWatcher<void>::finished, requestor,
+                   [on_ui_thread, watcher] {
+                     on_ui_thread();
+                     watcher->deleteLater();
+                   });
+  QFuture<void> future = QtConcurrent::run(&app.io_thread_pool, on_io_thread);
+  watcher->setFuture(future);
 }
--- src/io_task.h
+++ src/io_task.h
@@ -1,46 +1,24 @@
 #ifndef IOTASK_H
 #define IOTASK_H
 
-#include <QObject>
 #include <QtConcurrent>
 
-class BaseIoTask : public QObject {
-  Q_OBJECT
- public:
-  BaseIoTask();
-  void Run();
-
- protected:
-  virtual void RunInBackground() = 0;
-
- signals:
-  void finished();
-};
-
-template <typename T>
-class TemplateIoTask : public BaseIoTask {
- public:
-  explicit TemplateIoTask(const std::function<T()>& on_background)
-      : BaseIoTask(), on_background(on_background) {}
-
-  T result;
-
- protected:
-  void RunInBackground() { result = on_background(); }
-
- private:
-  std::function<T()> on_background;
-};
+#include "application.h"
 
 class IoTask {
  public:
   template <typename T>
   static void Run(QObject* requestor, const std::function<T()>& on_io_thread,
                   const std::function<void(T)>& on_ui_thread) {
-    auto task = new TemplateIoTask<T>(on_io_thread);
-    QObject::connect(task, &BaseIoTask::finished, requestor,
-                     [task, on_ui_thread] { on_ui_thread(task->result); });
-    task->Run();
+    Application& app = Application::Get();
+    auto watcher = new QFutureWatcher<T>();
+    QObject::connect(watcher, &QFutureWatcher<T>::finished, requestor,
+                     [on_ui_thread, watcher] {
+                       on_ui_thread(watcher->result());
+                       watcher->deleteLater();
+                     });
+    QFuture<T> future = QtConcurrent::run(&app.io_thread_pool, on_io_thread);
+    watcher->setFuture(future);
   }
 
   static void Run(QObject* requestor, const std::function<void()>& on_io_thread,
