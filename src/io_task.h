556f41469cbb805815436f52e1d4981c9a23db9e
gorolykmaxim <goroliuk.maksym@gmail.com>
2023-05-21 22:39:39 +0300

    Implement custom Promise class
    QFuture and it's then() has deal-breaking problems:
    - then(context, ...) call will try to execute callback even if
    context has been deleted (which is the default usecase for UI
    waiting for an async task to complete while being able to change
    at any time)
    - apparently you can't return QFuture inside then() and expect
    it to work like in JavaScript :)
    The new Promise class addresses both of these concerns.

--- src/io_task.h
+++ src/io_task.h
@@ -5,6 +5,53 @@
 
 #include "application.h"
 
+template <typename T>
+class Promise : public QFuture<T> {
+ public:
+  explicit Promise(const T& t) : QFuture<T>(QtFuture::makeReadyFuture(t)) {}
+  explicit Promise(T&& t) : QFuture<T>(QtFuture::makeReadyFuture(t)) {}
+  Promise(const QFuture<T>& another) : QFuture<T>(another) {}
+
+  void Then(QObject* ctx, std::function<void(T)>&& cb) const {
+    if (QFuture<T>::isFinished()) {
+      cb(QFuture<T>::result());
+      return;
+    }
+    auto w = new QFutureWatcher<T>(ctx);
+    QObject::connect(w, &QFutureWatcher<T>::finished, ctx,
+                     [cb = std::move(cb), w] {
+                       cb(w->result());
+                       w->deleteLater();
+                     });
+    w->setFuture(*this);
+  }
+
+  template <typename D>
+  Promise<D> Then(QObject* ctx, std::function<QFuture<D>(T)>&& cb) const {
+    auto qpd = QSharedPointer<QPromise<D>>::create();
+    if (QFuture<T>::isFinished()) {
+      Promise<D> d = cb(QFuture<T>::result());
+      d.Then(ctx, [qpd](D d) {
+        qpd->addResult(std::move(d));
+        qpd->finish();
+      });
+      return qpd->future();
+    }
+    auto w = new QFutureWatcher<T>(ctx);
+    QObject::connect(w, &QFutureWatcher<T>::finished, ctx,
+                     [cb = std::move(cb), qpd, w, ctx]() {
+                       Promise<D> d = cb(w->result());
+                       d.Then(ctx, [qpd](D d) {
+                         qpd->addResult(std::move(d));
+                         qpd->finish();
+                       });
+                       w->deleteLater();
+                     });
+    w->setFuture(*this);
+    return qpd->future();
+  }
+};
+
 class IoTask {
  public:
   template <typename T>
