5e36c712be01a13c517d5e0489103d2b8eb7ca0b
Maksym <goroliuk.maksym@gmail.com>
2023-06-27 00:22:21 +0300

    Slightly adjust build-win-release.bat

--- build-win-release.bat
+++ build-win-release.bat
@@ -3,7 +3,7 @@ call vcvars64.bat
 cmake -B build-release -G Ninja -DCMAKE_BUILD_TYPE=Release
 cmake --build build-release
 cd build-release
-rmdir /S /Q output
-mkdir output
-copy cpp-dev-tools.exe output
-%CMAKE_PREFIX_PATH%\bin\windeployqt.exe output -qmldir=..\qml
+rmdir /S /Q cpp-dev-tools
+mkdir cpp-dev-tools
+copy cpp-dev-tools.exe cpp-dev-tools
+%CMAKE_PREFIX_PATH%\bin\windeployqt.exe cpp-dev-tools -qmldir=..\qml
