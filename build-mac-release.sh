3fcb9edb884b5ee12375f8bdfde7c0c32b80e4f9
gorolykmaxim <goroliuk.maksym@gmail.com>
2023-06-26 15:43:00 +0300

    Release cpp-dev-tools 0.5 on Mac

---
+++ build-mac-release.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env zsh
+
+set -e
+cmake -B build-release -DCMAKE_OSX_ARCHITECTURES='arm64;x86_64' -DCMAKE_BUILD_TYPE=Release
+cmake --build build-release
+cd build-release
+rm -f cpp-dev-tools.dmg
+${CMAKE_PREFIX_PATH}/../../bin/macdeployqt cpp-dev-tools.app -qmldir=../qml -dmg
